39985b83234740b3291cc35322233dc8d8ad22a6
Lucas <lf-dias@outlook.com>
2024-11-21 16:36:33 -0300

    add chat fn

--- src/js/fn.js
+++ src/js/fn.js
@@ -1,23 +1,19 @@
-var clientMsg = $("#client_displayed_msg");
-var chatBotMsg = $("#chatbot_msg");
+var clientMsg = $(".client_msg_body");
+var chatBotMsg = $(".chatbot_msg_body");
 var btn = $("#send_btn")
-var clientInput = $('#client_input').val();
-var chatInput = $('#chatbot_msg').val();
+var chatInput = $('#chatbot_msg')
 
-
-if (clientInput == ''){
-    clientMsg.css("display", "block")
-} else {
-    clientMsg.css("display", "hidden")
+function sendMessage() {
+    msg = $('.client_input').val();
+    if ($.trim(msg) == '') {
+        return false;
+    }
+    $('<div class="client_displayed_msg">' + msg + '&nbsp;&nbsp; <i class="bi bi-menu-button-wide"></div>').appendTo($('.chat_std'));
+    $('<div class="chatbot_displayed_body"><i class="bi bi-robot"></i>&nbsp;&nbsp;' + "The answer to that question is: 42." + '</div>').appendTo($('.chat_std'));
+    $('.client_input').val(null);
+    $('.chatbot_msg_body').val('');
 }
 
-if (chatInput == ''){
-    chatInput.css("display", "block")
-} else {
-    chatInput.css("display", "hidden")
-}
-
-const inputParse = function(){
-    clientMsg.append('<div class="client_displayed_msg">'+ clientInput +'</div>')
-}
-window.addEventListener('click', inputParse)
+$(".client_input button").click(function() {
+    sendMessage();
+});
